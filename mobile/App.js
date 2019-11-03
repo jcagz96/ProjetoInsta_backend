64348c349d5faf0b96c66d6c5728815d0ee3fcae
jcagz96 <jcagz@iscte-iul.pt>
2019-11-03 20:14:50 +0000

    added mobile support

---
+++ mobile/App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { Text } from 'react-native';
+
+
+
+export default function App() {
+  return (
+    <Text>Olaaa mundo</Text>
+  );
+}
