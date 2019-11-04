db5f70063c983321480923168b008dd5cfde010e
jcagz96 <jcagz@iscte-iul.pt>
2019-11-04 23:13:27 +0000

    added async storage

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, Text, TextInput, AsyncStorage, TouchableOpacity, Image, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 
 import logo from '../assets/logo.png';
 import api from '../services/api';
@@ -11,61 +11,75 @@ export default function Login({ navigation }) {
     const [user, setUser] = useState('');
     const [password, setPassword] = useState('');
 
+    useEffect(() => {
+        AsyncStorage.getItem('sessionCookie').then(sessionCookie => {
+            if (sessionCookie) {
+                navigation.navigate('Profile');
+            }
+        });
+    }, []);
+
     async function handleLogin() {
         const response = await api.post('/login', {
             username: user,
             password: password,
         });
 
-        console.log('Bem-vindo, ', response.data.msg);
+        console.log('Bem-vindo, ', response.headers['set-cookie'][0]);
 
         if (response.data.msg === 'ok') {
-            navigation.navigate('Profile');
+            navigation.navigate('Profile', { username: user });
 
             // Decidir:   guardar username no asyncStorage? guardar cookies no async storage ou enviar pelo navigation ?
         }
     }
 
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView enabled={Platform.OS === 'android' || 'ios'} behavior="padding" style={styles.container}>
 
             <Image source={logo} />
 
-            <TextInput
-                placeholder="username"
-                placeholderTextColor="#999"
-                autoCapitalize="none"
-                autoCorrect={false}
-                style={styles.input}
-                value={user}
-                onChangeText={setUser}
-            />
+            <View style={styles.form}>
+                <TextInput
+                    placeholder="username"
+                    placeholderTextColor="#999"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    style={styles.input}
+                    value={user}
+                    onChangeText={setUser}
+                />
 
-            <TextInput
-                placeholder="password"
-                placeholderTextColor="#999"
-                style={styles.input}
-                autoCapitalize="none"
-                autoCorrect={false}
-                secureTextEntry={true}
-                value={password}
-                onChangeText={setPassword}
-            />
+                <TextInput
+                    placeholder="password"
+                    placeholderTextColor="#999"
+                    style={styles.input}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={setPassword}
+                />
 
-            <TouchableOpacity onPress={handleLogin}>
-                <Text>ola</Text>
-            </TouchableOpacity>
-        </View>
+                <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                    <Text style={styles.buttonText}>Encontrar spots</Text>
+                </TouchableOpacity>
+
+            </View>
+        </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#f5f5f5',
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 30,
+    },
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
     },
     input: {
         height: 46,
@@ -74,9 +88,20 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#ddd',
         borderRadius: 4,
-        marginTop: 20,
         paddingHorizontal: 15,
+        marginBottom: 10,
+    },
+    button: {
+        height: 46,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
 
-
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
     },
 });
